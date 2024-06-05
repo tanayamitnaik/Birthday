29c194cc8c4068fdd5ac3160ddea11e353a50ba3
tanayamitnaik <137375143+tanayamitnaik@users.noreply.github.com>
2024-06-05 23:15:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ const sname = document.getElementById("name")
 
 title.innerText = "Dear Love,";
 
-desc.innerText = "Hi pillu, ya veles letter pohochona possible navta so this is a virtual letter from me. Today you have turned 18 !!! Pn sotahala khup mottha samjhu nako. Tu nehemi majhi chotushi pilluch rahanar. Aaj parent tu sagle bdays la radli ahes,  pn ya veles nahi, From here onwards your all bdays will be different.
-I will change the whole vibe that you have about bdays. The most exciting
-is we will celebrate your bday in Nagpur & we will be together. I just want to tell you that, I will always be there with you, I love you the most. Happy Birthday ❤.";
+desc.innerText = "Hi pillu, ya veles letter pohochona possible navta so this is a virtual letter from me. Today you have turned 18 !!! Pn sotahala khup mottha samjhu nako. Tu nehemi majhi chotushi pilluch rahanar. Aaj parent tu sagle bdays la radli ahes,  pn ya veles nahi, From here onwards your all bdays will be different. I will change the whole vibe that you have about bdays. The most exciting is we will celebrate your bday in Nagpur & we will be together. I just want to tell you that, I will always be there with you, I love you the most. Happy Birthday ❤.";
 
 sname.innerText = "Tanay";
