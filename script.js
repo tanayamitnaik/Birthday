ad2528227d0e4f248e4267724c42b9ab2d631fea
tanayamitnaik <137375143+tanayamitnaik@users.noreply.github.com>
2024-06-05 22:26:14 +0530

    added files

---
+++ script.js
@@ -0,0 +1,11 @@
+const title = document.getElementById("title");
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+
+
+title.innerText = "Dear Love,";
+
+desc.innerText = "Hi pillu, Today you have turned 18 !!! Its a very special thing, and i will make it special, there is a lot more to come, when we will meet and celebrate it together, i just wanted to say that I love you, and i will always be there for you, Happy Birthday ❤.";
+
+sname.innerText = "Tanay";
